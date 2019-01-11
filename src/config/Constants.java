12fbf36dbbf26dd97eb8b430609ff9b4d1c4d2d1
Vatsal Sharma <Sharma_v@India.XoriantCorp.com>
2019-01-11 21:01:59 +0530

    Under Maintenance : working on testdata sheet and java files associated to make sure only requred cases run

--- src/config/Constants.java
+++ src/config/Constants.java
@@ -11,13 +11,16 @@ public class Constants {
 	public static final String path_chromeDriver = System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe";
 	public static final String path_OR = System.getProperty("user.dir") + "\\src\\config\\ObjectRepository.txt";
 	
+	
 	//column numbers for different fields
 	public static final int col_testcaseID = 0;
 	public static final int col_testStepID = 1;
 	public static final int col_ActionKeyword = 4;
 	public static final int col_pageObject = 3;
+	public static final int col_runMode = 2;
 	
 	//Test sheet name
 	public static final String sheet_testSteps = "TestSteps";
+	public static final String sheet_testCases = "TestCases";
 	
 }
--- src/utility/ExcelUtils.java
+++ src/utility/ExcelUtils.java
@@ -12,15 +12,44 @@ public class ExcelUtils {
 	private static XSSFSheet testSheet;
 	private static XSSFCell dataCell;
 	
-	public static void setExcelFile(String path, String sheetName) throws IOException {
+	public static void setExcelFile(String path) throws IOException {
 		FileInputStream testStream = new FileInputStream(path);
 		testBook = new XSSFWorkbook(testStream);
-		testSheet = testBook.getSheet(sheetName);
+		
 	}
 	
-	public static String getCellData(int rowNum,int colNum) {
+	public static String getCellData(int rowNum,int colNum, String sheetName) {
+		testSheet = testBook.getSheet(sheetName);
 		dataCell = testSheet.getRow(rowNum).getCell(colNum);
 		String tempData = dataCell.getStringCellValue();
 		return tempData;
+	}
+	
+	//function to get the row count in a sheet
+	public static int getRowCount(String sheetName)
+	{
+		testSheet = testBook.getSheet(sheetName);
+		int rowCount = testSheet.getLastRowNum() + 1;
+		return rowCount;
+	}
+	
+	//Method to get the row number of the Test case from the ** sheet
+	//takes three arguments TC_name,colNum,SheetName
+	public static int getRowContains(String testCaseName,int colNum,String sheetName) {
+		int lastRow = ExcelUtils.getRowCount(sheetName);
+		int i;
+		for(i=0;i<lastRow;i++)
+		{
+			if(ExcelUtils.getCellData(i, colNum, sheetName).equalsIgnoreCase(testCaseName))
+			break;
+		}
+		return i;
+	}
+	
+	//Method to return the count of testSteps in a particular testcase
+	//takes three arguments : sheetName,testCaseID,testcaseStart
+	public static void getTestStepsCount(String sheetName,String testCaseID,int startRow)
+	{
+		
 	}
 }
